7f71dd912d88f534e94fd8a06ab81ea89cd24991
Valeriya <molvasha@gmail.com>
2021-02-22 13:37:56 +0300

    init

---
+++ static_src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let messages = ['Привет', 'Как дела?'];
+const MessageComponent = (props) => <div>{props.text}</div>;
+const MessageField = (props) => {
+  return props.messages.map(message => <MessageComponent text={message}/>);
+};
+ReactDOM.render(
+  <MessageField messages={messages}/>,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require('webpack');
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './main.jsx',
+  },
+  context: path.resolve(__dirname, "static_src"),
+  output: {
+    path: path.resolve(__dirname, "static", "build"),
+    filename: 'app.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'static/build'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, "static_src"),
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/env', '@babel/react'],
+        }
+      },
+    ],
+  },
+};
